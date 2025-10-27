6015996da5280cba04093ae0e1104f946384d600
Arthur Barbosa <arthurbarbosaandrade2003@gmail.com>
2025-10-27 15:38:46 -0400

    Projeto Spring Boot
    Adição do projeto base com Spring Initializer

---
+++ Project/src/main/java/com/ThimoteoConsultorias/Consulthi/ConsulthiApplication.java
@@ -0,0 +1,13 @@
+package com.ThimoteoConsultorias.Consulthi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulthiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulthiApplication.class, args);
+	}
+
+}
---
+++ Project/src/test/java/com/ThimoteoConsultorias/Consulthi/ConsulthiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ThimoteoConsultorias.Consulthi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulthiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
